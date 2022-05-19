4318b8a0c5927cec87c63fb047b0ba81f4a59b4f
Alan Ferreira <alan.ferreira10@hotmail.com>
2022-05-18 22:00:26 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/alanferreira/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.alanferreira.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alanferreira.cursomc.domain.Categoria;
+import com.alanferreira.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
