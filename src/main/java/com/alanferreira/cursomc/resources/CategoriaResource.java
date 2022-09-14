6b8ab242ea22b8058f1de6925a4e3dbc80c9f183
Alan Ferreira <alan.ferreira10@hotmail.com>
2022-09-13 22:37:23 -0300

    inserindo uma nova Categoria com POST

--- src/main/java/com/alanferreira/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/alanferreira/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.alanferreira.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alanferreira.cursomc.domain.Categoria;
 import com.alanferreira.cursomc.services.CategoriaService;
@@ -24,4 +28,11 @@ public class CategoriaResource {
 		return ResponseEntity.ok(obj);
 	}
 
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
