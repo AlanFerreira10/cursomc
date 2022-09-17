6cc13911478e6291174fc19e462da5777484dca1
Alan Ferreira <alan.ferreira10@hotmail.com>
2022-09-16 22:59:43 -0300

    Personalizando o retorno da validacao

--- src/main/java/com/alanferreira/cursomc/resources/exceptions/StandardError.java
+++ src/main/java/com/alanferreira/cursomc/resources/exceptions/StandardError.java
@@ -3,7 +3,6 @@ package com.alanferreira.cursomc.resources.exceptions;
 import java.io.Serializable;
 
 public class StandardError implements Serializable{
-
 	private static final long serialVersionUID = 1L;
 	
 	private Integer status;
