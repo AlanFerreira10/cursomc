6d452aeef85fee5dc421e4fadac80d6d837657c9
Alan Ferreira <alan.ferreira10@hotmail.com>
2022-09-27 12:08:56 -0300

    Ajustando e testando delecao de Cliente

--- src/main/java/com/alanferreira/cursomc/services/ClienteService.java
+++ src/main/java/com/alanferreira/cursomc/services/ClienteService.java
@@ -56,7 +56,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
 	}
 
