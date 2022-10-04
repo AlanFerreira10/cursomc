0cb689f46a13a79ab8a4a5caed7b0f8758f7fd03
Alan Ferreira <alan.ferreira10@hotmail.com>
2022-10-04 15:05:12 -0300

    Criando profile de Desenvolvimento

---
+++ src/main/java/com/alanferreira/cursomc/config/DevConfig.java
@@ -0,0 +1,33 @@
+package com.alanferreira.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.alanferreira.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateDatabase();
+		return true;
+	}
+}
