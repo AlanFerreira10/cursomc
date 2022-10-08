dcf6f538d8d41c40ca7b7975c13c304e92603db3
Alan Ferreira <alan.ferreira10@hotmail.com>
2022-10-07 22:21:04 -0300

    implementando toString de Pedido

--- src/main/java/com/alanferreira/cursomc/domain/ItemPedido.java
+++ src/main/java/com/alanferreira/cursomc/domain/ItemPedido.java
@@ -1,6 +1,8 @@
 package com.alanferreira.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -108,6 +110,21 @@ public class ItemPedido implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		StringBuilder builder = new StringBuilder();
+		builder.append(getProduto().getNome());
+		builder.append(", Qtde: ");
+		builder.append(getQuantidade());
+		builder.append(", Preço Unitário: ");
+		builder.append(nf.format(getPreco()));
+		builder.append(", Subtotal: ");
+		builder.append(nf.format(getSubtotal()));
+		builder.append("\n");
+		return builder.toString();
 	}
 	
 	
