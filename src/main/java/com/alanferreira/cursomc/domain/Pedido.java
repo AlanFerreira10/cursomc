b60acd5b88b59f3621da591e2c8caf55f8fd7773
Alan Ferreira <alan.ferreira10@hotmail.com>
2022-10-07 22:22:58 -0300

    Corrigindo formatacao de data

--- src/main/java/com/alanferreira/cursomc/domain/Pedido.java
+++ src/main/java/com/alanferreira/cursomc/domain/Pedido.java
@@ -143,7 +143,7 @@ public class Pedido implements Serializable{
 	@Override
 	public String toString() {
 		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/aaaa HH:mm:ss");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		StringBuilder builder = new StringBuilder();
 		builder.append("Pedido número: ");
 		builder.append(getId());
