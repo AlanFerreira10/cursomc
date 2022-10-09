cf95eab02169741df1b83190d2e496a7158efe88
Alan Ferreira <alan.ferreira10@hotmail.com>
2022-10-09 19:21:21 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/alanferreira/cursomc/config/DevConfig.java
+++ src/main/java/com/alanferreira/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.alanferreira.cursomc.services.DBService;
+import com.alanferreira.cursomc.services.EmailService;
+import com.alanferreira.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 		
 		dbService.instantiateDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
