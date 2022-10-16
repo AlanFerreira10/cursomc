819ad42524e2b7818c7987846e8eab139b00a87c
Alan Ferreira <alan.ferreira10@hotmail.com>
2022-10-16 11:54:35 -0300

    Salvando perfis de usuário na base de dados

--- src/main/java/com/alanferreira/cursomc/services/DBService.java
+++ src/main/java/com/alanferreira/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.alanferreira.cursomc.domain.PagamentoComCartao;
 import com.alanferreira.cursomc.domain.Pedido;
 import com.alanferreira.cursomc.domain.Produto;
 import com.alanferreira.cursomc.domain.enums.EstadoPagamento;
+import com.alanferreira.cursomc.domain.enums.Perfil;
 import com.alanferreira.cursomc.domain.enums.TipoCliente;
 import com.alanferreira.cursomc.repositories.CategoriaRepository;
 import com.alanferreira.cursomc.repositories.CidadeRepository;
@@ -115,16 +116,21 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
 		Cliente cli1 = new Cliente(null, "Maria Silva", "ferreira.alan10@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
-
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
+		Cliente cli2 = new Cliente(null, "Ana Costa", "alan.ferreira10@gmail.com", "72398616002", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.addPerfil(Perfil.ADMIN);
+		cli2.getTelefones().addAll(Arrays.asList("98984422", "56229855"));
+		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
-
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "34577012", cli2, c2);
+		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
