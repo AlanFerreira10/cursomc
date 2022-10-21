eb441786da2cbbb40f4a0b005f48149ad4ccae18
Alan Ferreira <alan.ferreira10@hotmail.com>
2022-10-21 07:33:23 -0300

    implementando autorizacao

--- src/main/java/com/alanferreira/cursomc/config/SecurityConfig.java
+++ src/main/java/com/alanferreira/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.alanferreira.cursomc.security.JWTAuthenticationFilter;
+import com.alanferreira.cursomc.security.JWTAuthorizationFilter;
 import com.alanferreira.cursomc.security.JWTUtil;
 
 @Configuration
@@ -59,6 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
