db53b1bdf2df136931fbb1a321868598b17fd537
Alan Ferreira <alan.ferreira10@hotmail.com>
2022-11-01 11:32:28 -0300

    Endpoints para buscar estados e cidades

---
+++ src/main/java/com/alanferreira/cursomc/services/CidadeService.java
@@ -0,0 +1,20 @@
+package com.alanferreira.cursomc.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alanferreira.cursomc.domain.Cidade;
+import com.alanferreira.cursomc.repositories.CidadeRepository;
+
+@Service
+public class CidadeService {
+
+	@Autowired
+	private CidadeRepository repo;
+	
+	public List<Cidade> findByEstado(Integer estadoId){
+		return repo.findCidades(estadoId);
+	}
+}
